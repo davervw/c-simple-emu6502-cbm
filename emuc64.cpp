cb24d29fbd4b639cc6481817be5db1a58529250e
Dave Van Wagner <git@davevw.com>
2020-11-25 11:06:12 -0800

    Integrated USB Keyboard support
    USB to C64 keyboard portion from my STM32F429 port

--- emuc64.cpp
+++ emuc64.cpp
@@ -67,14 +67,193 @@
 #include "cbmconsole.h"
 
 #include "SPI.h"
-#include "ILI9341_t3.h"
-static ILI9341_t3 lcd = ILI9341_t3(10 /*CS*/, 9 /*DC*/);
+#include "ILI9341_t3n.h"
+#include "USBHost_t36.h"
 
 // globals
 char* StartupPRG = 0;
 
 // locals
 static int startup_state = 0;
+
+static ILI9341_t3n lcd = ILI9341_t3n(10 /*CS*/, 9 /*DC*/);
+static USBHost myusb;
+static USBHub hub1(myusb);
+static USBHub hub2(myusb);
+static KeyboardController keyboard1(myusb);
+static USBDriver *drivers[] = {&hub1, &hub2, &keyboard1};
+#define CNT_DEVICES (sizeof(drivers)/sizeof(drivers[0]))
+//static const char * driver_names[CNT_DEVICES] = {"Hub1","Hub2", "KB1"};
+static bool driver_active[CNT_DEVICES] = {false, false, false};
+
+// array allows multiple keys/modifiers pressed at one time
+static int scan_codes[9] = { 64, 64, 64, 64, 64, 64, 64, 64, 64 } ;
+
+// USB key code to C64 keyboard scan code, plus shift modifiers
+// +256 means to apply L.Shift
+// -512 means to take away Shift
+// +1024 means to apply RESTORE
+static int usb_to_c64[2][100] = {
+{ // normal/other modifier
+  64, 64, 64, 64, 10, 28, 20, 18, 14, 21, // na, na, na, na, a, b, c, d, e, f
+  26, 29, 33, 34, 37, 42, 36, 39, 38, 41, // g, h, i, j, k, l, m, n, o, p,
+  62, 17, 13, 22, 30, 31, 9, 23, 25, 12, // q, r, s, t, u, v, w, x, y, z
+  56, 59, 8, 11, 16, 19, 24, 27, 32, 35, // 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+  1, 63, 0, 58, 60, 43, 53, 256+45, 256+50, 48, // RET, STOP, DEL, CTRL, SPC, -, =, [, ], £
+  64, 50, 256+24, 64, 47, 44, 55, 64, 4, 256+4, // na, ;, ', na, ,, ., /, na, f1, f2
+  5, 256+5, 6, 256+6, 3, 256+3, 64, 64, 64, 64, // f3, f4, f5, f6, f7, f8, na, na, na, na
+  1024+64, 64, 63, 256+0, 51, 1024+64, 0, 64, 64, 2, // RESTORE, na, STOP, INS, HM, RESTORE, DEL, na, na, RT
+  256+2, 7, 256+7, 64, 55, 49, 43, 40, 1, 56, // LT, DN, UP, na, /, *, -, +, ENTER, 1
+  59, 8, 11, 16, 19, 24, 27, 32, 35, 44 // 2, 3, 4, 5, 6, 7, 8, 9, 0, . (keypad)
+},
+{ // shift modifier
+  64, 64, 64, 64, 10, 28, 20, 18, 14, 21, // na, na, na, na, a, b, c, d, e, f
+  26, 29, 33, 34, 37, 42, 36, 39, 38, 41, // g, h, i, j, k, l, m, n, o, p,
+  62, 17, 13, 22, 30, 31, 9, 23, 25, 12, // q, r, s, t, u, v, w, x, y, z
+  56, 512+46, 8, 11, 16, 512+54, 19, 512+49, 27, 32, // !, @, #, $, %, ^, &, *, (, )
+  1, 63, 0, 58, 60, 512+57, 512+40, 256+45, 256+50, 48, // RET, STOP, DEL, CTRL, SPC, L.Arrow, +, [, ], £
+  64, 512+45, 256+59, 64, 47, 44, 55, 64, 4, 256+4, // na, :, ", na, ,, ., /, na, f1, f2
+  5, 256+5, 6, 256+6, 3, 256+3, 64, 64, 64, 64, // f3, f4, f5, f6, f7, f8, na, na, na, na
+  1024+64, 64, 63, 256+0, 51, 1024+64, 0, 64, 64, 2, // RESTORE, na, STOP, INS, HM, RESTORE, DEL, na, na, RT
+  256+2, 7, 256+7, 64, 55, 49, 43, 40, 1, 56, // LT, DN, UP, na, /, *, -, +, ENTER, 1
+  59, 8, 11, 16, 19, 24, 27, 32, 35, 44 // 2, 3, 4, 5, 6, 7, 8, 9, 0, . (keypad)
+}
+};
+
+// Resources
+// See Keyboard/Keypad Page (0x07) of https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf
+// See Commodore 64 Keycodes in Appendix of https://archive.org/details/Compute_s_Mapping_the_64_and_64C/
+
+// The initial goal of the keyboard tables and logic is to provide access to at 
+// least the following keys from a standard USB keyboard, symbolic mapping more 
+// like a PC (not positional)
+// USB keyboard support is provided by USBHOST library and STM32F429 BSP
+//
+// STOP(ESC) F1 F2 F3 F4 F5 F6 F7 F8            Restore(PrtScr/SysRq) Run/Stop(Pause/Break)
+//           1! 2@ 3# 4$ 5% 6^ 7& 8* 9( 0) -_ += Del/Ins(Back)    Ins Hme/Clr     / * -
+// Ctrl(Tab) Q  W  E  R  T  Y  U  I  O  P  [  ]  £ (\)            Del           7 8 9 +
+//           A  S  D  F  G  H  J  K  L  ;: '" Return(ENTER)                     4 5 6
+// LShift    Z  X  C  V  B  N  M  ,< .> /?  RShift                     Up       1 2 3
+// C=(Ctrl)           SPACEBAR              C=(Ctrl)              Lft Down Rt   0 .   Enter
+//
+// Note C64 Ctrl key is the PC Tab key
+// Note C64 Commodore key is the PC Ctrl key
+// Note keys and modifiers not shown do nothing, not support for C64
+// Note PgUp is also Restore key
+//
+// Also, most Commodore key combinations should work for alphanumeric, some will 
+// be different/missing for punctuation, full mapping of PETSCII will require 
+// additional development
+
+// modifier at index zero, keys at indexes 2..7
+static uint8_t kbd_data[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+enum kbd_mod { lctrl=0x01, rctrl=0x10, lshift=0x02, rshift=0x20 }; // only tracking ones we care about
+
+// this method expects raw keyboard HID data buffer, specifically 8 length data
+static void onKeyData(uint8_t len, uint8_t* data)
+{
+//  static DigitalOut led2(LED2);
+//  led2 = !led2;
+//  
+//  Serial.printf("len=%d", len);
+//  for (int i=0; i<len; ++i)
+//     Serial.printf(" %02X", data[i]);
+//  Serial.printf("\n");
+  
+  if (len == 8)
+  { 
+    if ((data[0] & 0x11) != 0) // PC Ctrl => Commodore
+      scan_codes[6] = 61; // usb hid buffer supports 6 simultaneous keys, put modifier in next available slot
+    else
+      scan_codes[6] = 64;
+      
+    if ((data[0] & 2) != 0) // LShift
+      scan_codes[7] = 15; // usb hid buffer supports 6 simultaneous keys, put modifier in next available slot
+    else
+      scan_codes[7] = 64;
+      
+    if ((data[0] & 0x20) != 0) // RShift
+      scan_codes[8] = 52; // usb hid buffer supports 6 simultaneous keys, put modifier in next available slot
+    else
+      scan_codes[8] = 64;
+
+      for (int i=0; i<6; ++i)
+      {
+      if (data[i+2] < 100)
+      {
+        scan_codes[i] = usb_to_c64[((data[0] & 0x22) != 0) ? 1 : 0][data[i+2]]; // Normal vs. Shift
+        if ((scan_codes[i] & 256) != 0)
+          scan_codes[7] = 15; // LShift
+        if (i==0 && (scan_codes[i] & 512) != 0) // remove shift flag works only if key is first non-modifier pressed
+        {
+          scan_codes[7] = 64; // No LShift
+          scan_codes[8] = 64; // No RShift
+        }
+        if (scan_codes[i] != 64)
+        {
+//          printf(" %d", scan_codes[i]);
+//          fflush(stdout);
+        }
+      }
+      else
+        scan_codes[i] = 64;
+    }
+  }
+}
+
+// original code expected keyboard HID buffer, so reconstruct it from events we receive from our keyboard handler
+// place key into kbd_data indexes 2..7, or update modifier at index 0
+static void onKbdRawPress(int key)
+{
+  if (key == 0x68)
+    kbd_data[0] |= lshift;
+  else if (key == 0x6c)
+    kbd_data[0] |= rshift;
+  else if (key == 0x67)
+    kbd_data[0] |= lctrl;
+  else if (key == 0x6b)
+    kbd_data[0] |= rctrl;
+  else
+  {
+    int i=2;
+    while (i < 8 && kbd_data[i] != 0 && kbd_data[i] != key) // find key or end/slot
+      ++i;
+    if (i < 8 && kbd_data[i] == 0) // not found, have slot
+      kbd_data[i] = key; // store key
+  }
+  onKeyData(8, &kbd_data[0]);
+}
+
+// original code expected keyboard HID buffer, so reconstruct it from events we receive from our keyboard handler
+// remove key from kbd_data indexes 2..7, or update modifier at index 0
+static void onKbdRawRelease(int key)
+{
+  if (key == 0x68)
+    kbd_data[0] &= ~lshift;
+  else if (key == 0x6c)
+    kbd_data[0] &= ~rshift;
+  else if (key == 0x67)
+    kbd_data[0] &= ~lctrl;
+  else if (key == 0x6b)
+    kbd_data[0] &= ~rctrl;
+  else
+  {
+    int i=2;
+    while (i < 8 && kbd_data[i] != 0 && kbd_data[i] != key) // find key or end/slot
+      ++i;
+    if (i < 8 && kbd_data[i] == key) // found
+    {
+      while (i++ < 8)
+      {
+        if (i < 8)
+          kbd_data[i-1] = kbd_data[i];
+        else
+          kbd_data[i-1] = 0;
+      }
+    }
+  }
+  onKeyData(8, &kbd_data[0]);
+}
 
 //static void File_ReadAllBytes(byte* bytes, unsigned int size, const char* filename)
 //{
@@ -130,27 +309,27 @@ static int startup_state = 0;
 
 bool ExecutePatch(void)
 {
-	if (PC == 0xFFD2) // CHROUT
-	{
-		CBM_Console_WriteChar((char)A);
-		// fall through to draw character in screen memory too
-	}
-	else if (PC == 0xFFCF) // CHRIN
-	{
-		A = CBM_Console_ReadChar();
-
-		// SetA equivalent for flags
-		Z = (A == 0);
-		N = ((A & 0x80) != 0);
-		C = false;
-
-		// RTS equivalent
-		byte lo = Pop();
-		byte hi = Pop();
-		PC = (ushort)(((hi << 8) | lo) + 1);
-
-		return true; // overriden, and PC changed, so caller should reloop before execution to allow breakpoint/trace/ExecutePatch/etc.
-	}
+//	if (PC == 0xFFD2) // CHROUT
+//	{
+//		CBM_Console_WriteChar((char)A);
+//		// fall through to draw character in screen memory too
+//	}
+//	else if (PC == 0xFFCF) // CHRIN
+//	{
+//		A = CBM_Console_ReadChar();
+//
+//		// SetA equivalent for flags
+//		Z = (A == 0);
+//		N = ((A & 0x80) != 0);
+//		C = false;
+//
+//		// RTS equivalent
+//		byte lo = Pop();
+//		byte hi = Pop();
+//		PC = (ushort)(((hi << 8) | lo) + 1);
+//
+//		return true; // overriden, and PC changed, so caller should reloop before execution to allow breakpoint/trace/ExecutePatch/etc.
+//	}
 //	else if (PC == 0xA474) // READY
 //	{
 //		if (StartupPRG != 0 && strlen(StartupPRG) > 0) // User requested program be loaded at startup
@@ -906,6 +1085,10 @@ void C64_Init(void)
   // initialize ILI9341 LCD screen
   lcd.begin();
   lcd.fillScreen(ILI9341_BLACK);
+
+  myusb.begin();
+  keyboard1.attachRawPress(onKbdRawPress);
+  keyboard1.attachRawRelease(onKbdRawRelease);
 }
 
 // RGB565 colors picked with http://www.barth-dev.de/online/rgb565-color-picker/
@@ -994,25 +1177,25 @@ byte GetMemory(ushort addr)
       return chargen_rom[addr - io_addr];
     else if (addr >= color_addr && addr < color_addr + sizeof(color_nybles))
       return color_nybles[addr - color_addr] | 0xF0;
-//    else if (addr == 0xDC01)
-//    {
-//      int value = 0;
-//      
-//      for (int i=0; i<9; ++i)
-//      {
-//        int scan_code = scan_codes[i] & 127; // remove any modifiers
-//        if (scan_code < 64)
-//        {     
-//          int col = scan_code / 8;
-//          int row = scan_code % 8;
-//          
-//          if ((io[0xC00] & (1 << col)) == 0)
-//            value |= (1 << row);
-//        }
-//      }
-//      
-//      return ~value;
-//    }
+    else if (addr == 0xDC01)
+    {
+      int value = 0;
+      
+      for (int i=0; i<9; ++i)
+      {
+        int scan_code = scan_codes[i] & 127; // remove any modifiers
+        if (scan_code < 64)
+        {     
+          int col = scan_code / 8;
+          int row = scan_code % 8;
+          
+          if ((io[0xC00] & (1 << col)) == 0)
+            value |= (1 << row);
+        }
+      }
+      
+      return ~value;
+    }
     else
       return io[addr - io_addr];
   }
